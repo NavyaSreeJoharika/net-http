d0bcb53b1aa0a3ff340d9fc138f891cb292d3bf6
NavyaSreeJohairka <navyasreejohairka@gmail.com>
2024-07-24 14:33:19 +0530

    initial commit

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 */
-// /*
+/*
 package main
 
 import (
@@ -65,4 +65,41 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-// */
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello World")
+
+}
+
+func sum(w http.ResponseWriter, r *http.Request) {
+	x, y := 10, 20
+
+	sum_of_values := x + y
+
+	fmt.Fprintf(w, "Sum of 10 and 20 is %d", sum_of_values)
+}
+
+func main() {
+	fmt.Println("Server is runining on port 8080")
+
+	http.Handle("/",
+		http.HandlerFunc(
+			index,
+		),
+	)
+
+	http.HandleFunc("/sum_of_numbers", sum)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
